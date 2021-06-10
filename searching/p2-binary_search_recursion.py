782eeac3d7dc299d9d03657fa1a3cc232b7c54ed
Youngjin Jo <quasarhub@gmail.com>
2021-06-10 23:11:06 +0900

    [Add] Binary Search using Recursion

---
+++ searching/p2-binary_search_recursion.py
@@ -0,0 +1,22 @@
+def binary_search(array, target, start, end):
+    # 종료 조건
+    if start > end:
+        return None
+
+    mid = (start + end) // 2
+
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+
+array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+target = 9
+n = len(array)
+print(binary_search(array, target, 0, n))
